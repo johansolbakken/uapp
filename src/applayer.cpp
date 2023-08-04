3c2fe81d308418b2891528fc40c44b2306c211dd
Johan Solbakken <johsol@stud.ntnu.no>
2023-08-04 14:30:47 +0200

    Application close

--- src/applayer.cpp
+++ src/applayer.cpp
@@ -6,9 +6,13 @@ void AppLayer::onAttach()
 {
     std::cout << "AppLayer::onAttach()" << std::endl;
 }
+
 void AppLayer::onDetach()
 {
+    std::cout << "AppLayer::onDetach()" << std::endl;
 }
+
 void AppLayer::onUpdate()
 {
+    Updateable::Application::instance().close();
 }
